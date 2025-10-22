e37eb2ccd5894906fc33431006205af4d1069974
APAC\gbb1hc <gbb1hc@bosch.com>
2025-10-22 12:19:11 +0700

    update ui for mini player button and update display logic for new youtube controls

--- content.js
+++ content.js
@@ -3,7 +3,15 @@ function addMiniPlayerButton() {
         return;
     }
 
-    const rightControls = document.querySelector('.ytp-right-controls');
+    const rightControls = document.querySelector('.ytp-right-controls-right');
+
+    // const newImageInImageButton = document.querySelector('.ytp-pip-button');
+
+    // newImageInImageButton.setAttribute('style', '');
+
+    // const newImageInImageButtonSvg = newImageInImageButton.querySelector('svg');
+
+    // newImageInImageButtonSvg.setAttribute('style', 'width: 48px; height: 40px; padding: 0;');
     
     if (!rightControls) {
         setTimeout(addMiniPlayerButton, 500);
@@ -12,18 +20,17 @@ function addMiniPlayerButton() {
 
     const newButton = document.createElement('button');
     newButton.id = 'custom-miniplayer-button';
-    newButton.className = 'ytp-button';
-    newButton.title = 'Trình phát thu nhỏ (phím tắt I)'; // Tooltip khi rê chuột
+    newButton.classList.add('ytp-button', 'ytp-fullscreen-button')
+    newButton.setAttribute('title', '');
+    newButton.setAttribute('data-tooltip-title',  'Trình phát thu nhỏ (i)');
+    newButton.setAttribute('aria-label',  'Trình phát thu nhỏ (i)');
+    newButton.setAttribute('data-title-no-tooltip',  'Trình phát thu nhỏ (i)');
+    newButton.setAttribute('data-priority', 13)
 
     newButton.innerHTML = `
-        <svg 
-            height="100%" 
-            version="1.1" 
-            viewBox="0 0 36 36" 
-            width="100%"
-            style="transform: scale(0.70); transform-origin: center;"
-        >
-            <path fill="#fff" d="M25,17 L17,17 L17,25 L25,25 L25,17 L25,17 Z M29,29 L7,29 L7,7 L29,7 L29,29 Z M27,9 L9,9 L9,27 L27,27 L27,9 L27,9 Z"></path>
+       <svg height="36" version="1.1" viewBox="0 0 36 36" width="36" style="width: 48px; height: 40px; padding: 0;"">
+            <use class="ytp-svg-shadow" xlink:href="#ytp-id-17"></use>
+            <path d="M25,17 L17,17 L17,23 L25,23 L25,17 L25,17 Z M29,25 L29,10.98 C29,9.88 28.1,9 27,9 L9,9 C7.9,9 7,9.88 7,10.98 L7,25 C7,26.1 7.9,27 9,27 L27,27 C28.1,27 29,26.1 29,25 L29,25 Z M27,25.02 L9,25.02 L9,10.97 L27,10.97 L27,25.02 L27,25.02 Z" fill="#fff"></path>
         </svg>
     `;
 
@@ -40,10 +47,10 @@ function addMiniPlayerButton() {
 
     const fullScreenButton = rightControls.querySelector('.ytp-fullscreen-button');
 
-    if (fullScreenButton) {
+    if (fullScreenButton && fullScreenButton.parentNode === rightControls) {
         rightControls.insertBefore(newButton, fullScreenButton);
     } 
-    else {
+    else if (rightControls) {
         rightControls.appendChild(newButton);
     }
 }
